55fe60620b6cdbe332d810b4a1e2030027eb6dbb
Rafael Guibson <rafaelguibsons@gmail.com>
2025-08-11 16:15:09 -0300

    LinkedList constructor's method implemented

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,29 @@
+package linkedlist;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void append(int value) {}
+
+    public void prepend(int value) {}
+
+    public void insert(int index, int value) {}
+}
---
+++ linkedlist/Main.java
@@ -0,0 +1,9 @@
+package linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList newLinkedList = new LinkedList(4);
+        System.out.println(newLinkedList);
+
+    }
+}
